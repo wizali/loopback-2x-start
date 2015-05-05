d33660b51fc61c0d3d5d2509bbc6c4e58cfe6045
wizali <wiza.liu@gmail.com>
2015-05-05 00:30:18 -0700

    20150505-2

--- client/app/modules/auth/teacher/controllers/teacher.ctrl.js
+++ client/app/modules/auth/teacher/controllers/teacher.ctrl.js
@@ -1,18 +1,32 @@
 'use strict';
 
 angular.module('app.auth.teacher')
-.controller('teacherCtrl',['$scope','$rootScope','authServ',function ($scope,$rootScope,authServ){
+    .controller('teacherCtrl', ['$scope', '$rootScope', 'authServ', function ($scope, $rootScope, authServ) {
 
-	$scope.theachaerList = [];
+        $rootScope.teacher = {
+            add_teacher: true,
+            edit_teacher: ''
+        };
 
-//	var Teacher = authServ.teacher;
-//
-//	Teacher.query()
-//	.success(function (data){
-//		$scope.theachaerList = data;
-//	})
-//	.error(function (err){
-//		$.tips(err,'error');
-//	})
+        $scope.theachaerList = [];
 
-}]);
+        var User = authServ.user,
+            Role_User = authServ.role_user;
+
+        Role_User.query({include:['user','role']})
+            .success(function (data){
+                angular.forEach(data,function (d){
+                    if (d.role.name === '老师'){
+                        $scope.theachaerList.push(d.user);
+                    }
+                });
+            })
+            .error(function (err){
+                $.tips(err,'error');
+            });
+
+
+        $scope.addTeacher = function () {
+
+        }
+    }]);
